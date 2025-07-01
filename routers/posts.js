d1144e6ca2a95559f7279c0d387d5ac2bf03fe3e
GianniC88 <gianni.colucci.88@gmail.com>
2025-07-01 22:45:02 +0200

    Aggiunta la funzionalità di eliminazione dei post tramite ID

--- routers/posts.js
+++ routers/posts.js
@@ -16,3 +16,32 @@ router.get('/:id', (req, res) => {
 	res.json(post);
 });
 module.exports = router;
+
+//destroy
+router.delete('/:id', (req, res) => {
+	//recupero d'id dal'URL e lo trasformo in numero
+	const id = parseInt(req.params.id)
+
+	//cerchiamo il post tramite id
+	const post = posts.find(post => post.id === id);
+
+	//controllo
+	if (!post) {
+		res.status(404);
+
+		return res.json({
+			status: 404,
+			error: "not found",
+			message: "Post non trovato"
+		})
+	}
+	//rimozione post n°
+	posts.splice(posts.indexOf(post), 1)
+
+	console.log(posts)
+
+	res.sendStatus(204)
+})
+
+
+//
